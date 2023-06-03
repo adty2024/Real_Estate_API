c35a5e9d7eb7d447d558a0def90f1bb110c1ace3
adty2024 <avisaxena3019@gmail.com>
2023-06-03 08:11:15 +0530

    Added 2 links in cors

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import h5py
 import pickle
 import base64
+from flask_cors import CORS
 
 with h5py.File('model.h5', 'r') as f:
     model_string = f['model'][()]
@@ -10,6 +11,7 @@ with h5py.File('model.h5', 'r') as f:
 
 # Create a Flask app
 app = Flask(__name__)
+CORS(app, origins=['http://127.0.0.1:5173', 'https://real-estate-react-nine.vercel.app'])
 
 @app.route("/predict/<string:one>/<string:two>/<string:three>/<string:four>/<string:five>/<string:six>/<string:seven>/<string:eight>", methods=["GET"])
 def predict(one, two, three, four, five, six, seven, eight):
